7b98a9fd3daa7cc9d24a46926a989e4f1b4be8af
choi-ys <project.log.062@gmail.com>
2024-10-13 18:32:04 +0900

    feat: toss payments 결제 승인 API 호출을 위한 web-client 기반 환경 설정 구성

--- src/main/java/com/qticket/payment/PaymentApplication.java
+++ src/main/java/com/qticket/payment/PaymentApplication.java
@@ -1,9 +1,12 @@
 package com.qticket.payment;
 
+import com.qticket.payment.adapter.out.web.config.webclient.toss.TossPaymentsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TossPaymentsProperties.class)
 public class PaymentApplication {
 
     public static void main(String[] args) {
