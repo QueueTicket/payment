92e774334f6c0b7e83c94716323cba86c4cf03bc
choi-ys <project.log.062@gmail.com>
2024-10-16 22:14:49 +0900

    chore: polishing flyway table init script

--- src/main/resources/db/migration/V1__Initialize_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_tables.sql
@@ -1,6 +1,6 @@
-create table payment_event
+CREATE TABLE payment_event
 (
-    id           BIGINT                              NOT NULL AUTO_INCREMENT,
+    id           BIGINT                              AUTO_INCREMENT PRIMARY KEY,
     customer_id  BIGINT                              NOT NULL,
     order_id     VARCHAR(255) UNIQUE,
     order_name   VARCHAR(255)                        NOT NULL,
@@ -9,12 +9,11 @@ create table payment_event
     is_completed BOOLEAN                             NOT NULL DEFAULT FALSE,
     approved_at  DATETIME,
     fail_count   INT                                 NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
 ) engine = InnoDB;
 
-create table payment_order
+CREATE TABLE payment_order
 (
-    id                      BIGINT                                                                 NOT NULL AUTO_INCREMENT,
+    id                      BIGINT                                                                 AUTO_INCREMENT PRIMARY KEY,
     payment_event_id        BIGINT                                                                 NOT NULL,
     customer_id             BIGINT                                                                 NOT NULL,
     order_id                VARCHAR(255)                                                           NOT NULL,
@@ -25,16 +24,15 @@ create table payment_order
     status                  ENUM ('PENDING', 'PROCESSING', 'SUCCESS', 'FAILED', 'UNKNOWN_APPROVE') NOT NULL DEFAULT 'PENDING',
     is_ledger_completed     BOOLEAN                                                                NOT NULL DEFAULT FALSE,
     is_settlement_completed BOOLEAN                                                                NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id),
-    FOREIGN KEY (payment_event_id) references payment_event (id)
+    FOREIGN KEY (payment_event_id) REFERENCES payment_event (id)
 ) engine = InnoDB;
 
-create table payment_order_history
+CREATE TABLE payment_order_history
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     payment_order_id BIGINT NOT NULL,
     previous_status  ENUM ('PENDING', 'PROCESSING', 'SUCCESS', 'FAILED', 'UNKNOWN_APPROVE'),
     new_status       ENUM ('PENDING', 'PROCESSING', 'SUCCESS', 'FAILED', 'UNKNOWN_APPROVE'),
     reason           VARCHAR(255),
-    FOREIGN KEY (payment_order_id) references payment_order (id)
+    FOREIGN KEY (payment_order_id) REFERENCES payment_order (id)
 )
